604e99c1b9d2fcba7c799a871cc5e222058a7cba
Fejiro001 <aberefejiro@gmail.com>
2026-08-05 17:55:38 -0500

    Added properties to the Speaker model

--- TechConfCentral.Models/Speaker.cs
+++ TechConfCentral.Models/Speaker.cs
@@ -2,5 +2,13 @@
 {
     public class Speaker
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string Company { get; set; }
+        public string Biography { get; set; }
+        public string? ProfileImage { get; set; }
+        public bool IsFeatured { get; set; } = false;
     }
 }
