e581a86a17cd1a1e9832f70beee1fde2606761c4
Fejiro001 <aberefejiro@gmail.com>
2026-08-05 18:11:07 -0500

    Created a DbContext and added models as dbsets to the dbcontext

---
+++ TechConfCentral.DAL/TechConfCentralContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechConfCentral.Models;
+
+namespace TechConfCentral.DAL
+{
+    public class TechConfCentralContext : DbContext
+    {
+        public DbSet<Conference> Conferences { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+        public DbSet<Speaker> Speakers { get; set; }
+        public DbSet<Talk> Talks { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<SavedTalk> SavedTalks { get; set; }
+        public TechConfCentralContext(DbContextOptions<TechConfCentralContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
