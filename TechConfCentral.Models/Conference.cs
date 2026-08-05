b0f3e1f8f70ac53f45d25ad9cc231b3f0e769493
Fejiro001 <aberefejiro@gmail.com>
2026-08-05 17:54:06 -0500

    Added properties to the Conference model

--- TechConfCentral.Models/Conference.cs
+++ TechConfCentral.Models/Conference.cs
@@ -2,5 +2,15 @@
 {
     public class Conference
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Tagline { get; set; }
+        public string? Description { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public string Venue { get; set; }
+        public string City { get; set; }
+        public string StateOrProvince { get; set; }
+        public string Country { get; set; }
     }
 }
