838a44d6f4a22ca6dd2b273375abe32ed79d5350
Fejiro001 <aberefejiro@gmail.com>
2026-08-05 17:57:51 -0500

    Added properties to the SavedTalk join model

--- TechConfCentral.Models/SavedTalk.cs
+++ TechConfCentral.Models/SavedTalk.cs
@@ -2,5 +2,11 @@
 {
     public class SavedTalk
     {
+        public int Id { get; set; }
+        public DateTime SavedAt { get; set; }
+        // Foreign Keys
+        public int TalkId { get; set; }
+        public Talk Talk { get; set; }
+        // TODO: Add User Identity key when ready
     }
 }
