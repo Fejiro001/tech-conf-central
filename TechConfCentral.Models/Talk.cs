5a9f9d463439fb7f9bb819e5da1a0bd59caf8322
Fejiro001 <aberefejiro@gmail.com>
2026-08-05 17:54:56 -0500

    Added properties to the Track model

--- TechConfCentral.Models/Talk.cs
+++ TechConfCentral.Models/Talk.cs
@@ -2,5 +2,22 @@
 {
     public class Talk
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+        public bool IsFeatured { get; set; } = false;
+        public bool IsKeynote { get; set; } = false;
+        // Foreign Keys
+        public int ConferenceId { get; set; }
+        public Conference Conference { get; set; }
+        public int TrackId { get; set; }
+        public Track Track { get; set; }
+        public int RoomId { get; set; }
+        public Room Room { get; set; }
+        public int SpeakerId { get; set; }
+        public Speaker Speaker { get; set; }
+        public ICollection<SavedTalk> SavedTalks { get; set; } = new List<SavedTalk>();
     }
 }
