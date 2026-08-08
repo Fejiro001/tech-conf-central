2664daff3ec1560ac093b4749067f52535e2b398
Fejiro001 <aberefejiro@gmail.com>
2026-08-07 23:22:26 -0500

    Implemented ConferenceRepository in TechConfCentral.DAL using EF Core. Added async methods for getting, adding, updating, and deleting Conference entities.

---
+++ TechConfCentral.DAL/ConferenceRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechConfCentral.Models;
+
+namespace TechConfCentral.DAL
+{
+    public class ConferenceRepository
+    {
+        private readonly TechConfCentralContext _context;
+        public ConferenceRepository(TechConfCentralContext context)
+        {
+            _context = context;
+        }
+        // Get all conference
+        public async Task<List<Conference>> GetConferencesAsync()
+        {
+            return await _context.Conferences.ToListAsync();
+        }
+        // Get conference by id
+        public async Task<Conference> GetConferenceByIdAsync(int id)
+        {
+            return await _context.Conferences.FindAsync(id);
+        }
+        // Create conference
+        public async Task AddConferenceAsync(Conference conference)
+        {
+            await _context.Conferences.AddAsync(conference);
+        }
+        // Update conference
+        public void UpdateConference(Conference conference)
+        {
+            _context.Conferences.Update(conference);
+        }
+        // Delete conference
+        public async Task DeleteConferenceAsync(int id)
+        {
+            Conference? conference = await _context.Conferences.FindAsync(id);
+            if (conference != null)
+            {
+                _context.Conferences.Remove(conference);
+            }
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
