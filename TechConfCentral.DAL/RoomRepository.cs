f0e76d72bc154b86e8d7b35cbcb3455e4cb0a160
Fejiro001 <aberefejiro@gmail.com>
2026-08-08 13:13:40 -0500

    Implemented RoomRepository in DAL for Room entities using EF Core. Added async methods for Get, Add, Update, Delete, and SaveChanges.

---
+++ TechConfCentral.DAL/RoomRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechConfCentral.Models;
+
+namespace TechConfCentral.DAL
+{
+    public class RoomRepository
+    {
+        private readonly TechConfCentralContext _context;
+        public RoomRepository(TechConfCentralContext context)
+        {
+            _context = context;
+        }
+        // Get all rooms
+        public async Task<List<Room>> GetRoomsAsync()
+        {
+            return await _context.Rooms.ToListAsync();
+        }
+        // Get room by id
+        public async Task<Room?> GetRoomByIdAsync(int id)
+        {
+            return await _context.Rooms.FindAsync(id);
+        }
+        // Create room
+        public async Task AddRoomAsync(Room room)
+        {
+            await _context.Rooms.AddAsync(room);
+        }
+        // Update room
+        public void UpdateRoom(Room room)
+        {
+            _context.Rooms.Update(room);
+        }
+        // Delete room
+        public async Task DeleteRoomAsync(int id)
+        {
+            Room? room = await _context.Rooms.FindAsync(id);
+            if (room != null)
+            {
+                _context.Rooms.Remove(room);
+            }
+        }
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
