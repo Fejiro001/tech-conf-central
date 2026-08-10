26207fe3a473613db6f3a6245956c3ee37af5887
Fejiro001 <aberefejiro@gmail.com>
2026-08-10 15:06:36 -0500

    Added  the SavedTalkService in TechConfCentral.BLL to handle retrieving, saving (with duplicate checks), and removing saved talks for users.

---
+++ TechConfCentral.BLL/SavedTalkService.cs
@@ -0,0 +1,37 @@
+﻿using TechConfCentral.DAL;
+using TechConfCentral.Models;
+
+namespace TechConfCentral.BLL
+{
+    public class SavedTalkService
+    {
+        private readonly SavedTalkRepository _repository;
+        public SavedTalkService(SavedTalkRepository repository)
+        {
+            _repository = repository;
+        }
+        // Get all Saved Talks
+        public async Task<List<SavedTalk>> GetSavedTalksForUserAsync(string userId)
+        {
+            return await _repository.GetSavedTalksForUserAsync(userId);
+        }
+        // Save a Talk
+        public async Task SaveTalkAsync(SavedTalk savedtalk)
+        {
+            bool isTalkSaved = await _repository.IsTalkSavedAsync(savedtalk.UserId, savedtalk.TalkId);
+
+            if (isTalkSaved)
+            {
+                throw new Exception("Talk has already been saved.");
+            }
+            await _repository.SaveTalkAsync(savedtalk);
+            await _repository.SaveChangesAsync();
+        }
+        // Delete Saved Talk
+        public async Task RemoveSavedTalkAsync(int id)
+        {
+            await _repository.RemoveSavedTalkAsync(id);
+            await _repository.SaveChangesAsync();
+        }
+    }
+}
