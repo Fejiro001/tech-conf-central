246603fd55a3f8e2b20ebe662c5630bbc4f374a7
Fejiro001 <aberefejiro@gmail.com>
2026-08-11 16:46:42 -0500

    Added unique indexes to the Name columns of Track and Room tables.

---
+++ TechConfCentral.DAL/Migrations/20260811214401_AddedUniqueConstraints.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechConfCentral.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedUniqueConstraints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Track_Name",
+                table: "Track",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Room_Name",
+                table: "Room",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Track_Name",
+                table: "Track");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Room_Name",
+                table: "Room");
+        }
+    }
+}
