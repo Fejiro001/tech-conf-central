f760ef230e03e0348af9831f4b8fa3bc2956e002
Fejiro001 <aberefejiro@gmail.com>
2026-08-11 09:08:00 -0500

    Added a ConferenceController to handle conference details view, depending on ConferenceService, TalkService, TrackService, and SpeakerService. Add ConferenceViewModel to combine conference data for the view.

---
+++ TechConfCentral/Controllers/ConferenceController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechConfCentral.BLL;
+using TechConfCentral.Models;
+
+namespace TechConfCentral.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        private readonly ConferenceService _conferenceService;
+        private readonly SpeakerService _speakerService;
+        private readonly TalkService _talkService;
+        private readonly TrackService _trackService;
+
+        public ConferenceController(ConferenceService conferenceService, TalkService talkService, TrackService trackService)
+        {
+            _conferenceService = conferenceService;
+            _talkService = talkService;
+            _trackService = trackService;
+        }
+        // View a single conference details
+        public async Task<IActionResult> Index(int conferenceId)
+        {
+            var vm = new ConferenceViewModel
+            {
+                Conference = await _conferenceService.GetConferenceByIdAsync(conferenceId),
+                FeaturedKeynote = await _talkService.GetKeynoteTalkAsync(conferenceId),
+                Tracks = await _trackService.GetTracksByConferenceAsync(conferenceId),
+                FeaturedSpeakers = await _speakerService.GetFeaturedSpeakersAsync(conferenceId),
+                FeaturedTalks = await _talkService.GetFeaturedTalksAsync(conferenceId)
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ TechConfCentral/Models/ConferenceViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TechConfCentral.Models
+{
+    public class ConferenceViewModel
+    {
+        public Conference? Conference { get; set; } = null;
+        public Talk? FeaturedKeynote { get; set; }
+        public List<Track> Tracks { get; set; } = [];
+        public List<Speaker> FeaturedSpeakers { get; set; } = [];
+        public List<Talk> FeaturedTalks { get; set; } = [];
+    }
+}
